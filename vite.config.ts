d8b92abb37d32e6281a27402c98d2d77278e766f
Dror Handler <hdror507@gmail.com>
2024-06-07 01:10:26 +0300

    index fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-base:'/reactTS-mmatrix/',
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   },
-  // },
+  base: '/reactTS-matrix/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
