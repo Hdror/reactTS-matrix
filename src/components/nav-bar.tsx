f68b3b083a1095faf2b5692c19f3434ef54dc9f1
Dror Handler <hdror507@gmail.com>
2024-06-06 23:32:32 +0300

    add avatar in nav bar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -14,6 +14,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { Avatar } from '@mui/material';
 
 interface Props { }
 
@@ -66,7 +67,7 @@ export default function NavBar(props: Props) {
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
-            MUI
+            <Avatar>DH</Avatar>
           </Typography>
           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
             {navItems.map((item,idx) => (
