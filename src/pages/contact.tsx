9c855eaec391bdb5b08c347571ab52cec4da93ea
Dror Handler <hdror507@gmail.com>
2024-06-06 21:05:41 +0300

    style page-headline

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material";
 export default function Contact() {
     return (
         <Box>
-            <Typography className="page-headline" sx={{ typography: { sm: 'h1', xs: 'h6' } }}  variant="h1">Contact page</Typography>
+            <h1 className="page-headline">Contact page</h1>
         </Box>
     )
 }
