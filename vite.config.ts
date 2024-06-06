8eaf3086a634c5d4fd60f0412aa15c008bb38fff
Dror Handler <hdror507@gmail.com>
2024-06-07 00:38:10 +0300

    package update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/reactTS-matrix/',
+  // base:'/reactTS-matrix/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
